378604f1f6ed4cfde8944a77c6f203a33a27e912
gideb <gideblarbi@gmail.com>
2026-02-19 17:01:32 +0000

    style contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactSection = () => {
+  const [success, setSuccess] = useState(false);
+
+  const formSubmit = () => {
+    setSuccess(true);
+  };
+
   return (
     <div id="contact">
       <section
         id="why-us"
-        className="px-6 sm:px-12 py-24 text-center bg-linear-to-b from-purple-50 via-white to-white
+        className="relative px-6 sm:px-12 py-24 text-center bg-linear-to-b from-purple-50 via-white to-white
 "
       >
+        {/* Background Glow */}
+        <div className="absolute inset-0 -z-10">
+          <div
+            className="absolute top-20 left-1/2 -translate-x-1/2 w-125 h-125 bg-purple-500/10 
+  blur-[120px] rounded-full"
+          ></div>
+        </div>
+
         <div className="max-w-7xl mx-auto min-h-60">
           <div className="inline-flex items-center gap-2 bg-purple-100 text-purple-600 px-4 py-2 rounded-full text-sm font-medium w-fit mx-auto">
             <span className="w-2 h-2 bg-fuchsia-600 rounded-full animate-pulse"></span>
@@ -101,43 +115,41 @@ w-10 h-10 flex items-center justify-center rounded-xl shadow-md group-hover:scal
             </div>
 
             <div
-              className="mt-10 bg-white/80 backdrop-blur-xl p-10 rounded-3xl 
+              className="mt-10 bg-white/80 backdrop-blur-xl px-10 pt-6  rounded-3xl 
 shadow-[0_20px_60px_rgba(124,58,237,0.15)] border border-purple-100"
             >
-              <form className="flex flex-col text-left space-y-4">
-                <div className="flex">
-                  <div className="grid grid-cols-2 gap-5">
-                    <div className="flex flex-col gap-2 ">
-                      <label htmlFor="name">Name</label>
-                      <input
-                        type="text"
-                        placeholder="Your full name"
-                        className=" bg-white border border-purple-200 rounded-xl px-4 py-3
-focus:outline-none focus:ring-2 focus:ring-purple-500/40 
-focus:border-purple-500 transition"
-                      />
-                    </div>
+              <form
+                className="flex flex-col text-left pt-10 pb-0 space-y-4 animate-[float_6s_ease-in-out_infinite]"
+                onSubmit={formSubmit}
+              >
+                <div className="relative flex flex-col gap-2  ">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    placeholder="Your name"
+                    className="bg-white border border-purple-200 rounded-xl pl-12 py-3 w-full     focus:outline-none focus:ring-2 focus:ring-purple-500/40          focus:border-purple-500 transition"
+                  />
+                  <i className="fas fa-user absolute left-4 top-1/2 translate-y-1/2 text-gray-400"></i>
+                </div>
 
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="email">Email</label>
-                      <input
-                        type="email"
-                        placeholder="Your@email"
-                        className="bg-white border border-purple-200 rounded-xl px-4 py-3
-focus:outline-none focus:ring-2 focus:ring-purple-500/40 
-focus:border-purple-500 transition"
-                      />
-                    </div>
-                  </div>
+                <div className="relative flex flex-col gap-2 ">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    placeholder="Your@email"
+                    className="bg-white border border-purple-200 rounded-xl pl-12 py-3 w-full
+               focus:outline-none focus:ring-2 focus:ring-purple-500/40
+               focus:border-purple-500 transition"
+                  />
+                  <i className="fas fa-envelope absolute left-4 top-1/2 translate-y-1/2 text-gray-400"></i>
                 </div>
 
                 <label htmlFor="service">Service Type</label>
                 <select
                   name="service"
                   id="service"
-                  className="bg-white border border-purple-200 rounded-xl px-4 py-3
-focus:outline-none focus:ring-2 focus:ring-purple-500/40 
-focus:border-purple-500 transition"
+                  className=" bg-white border border-purple-200 rounded-xl px-4 py-3
+focus:outline-none focus:ring-2 focus:ring-purple-500/40 focus:border-purple-500 transition"
                 >
                   <option value="">Select Service Type</option>
                 </select>
@@ -149,21 +161,23 @@ focus:border-purple-500 transition"
                   placeholder="Tell us about your project"
                   rows={4}
                   className="bg-white border border-purple-200 rounded-xl px-4 py-3
-focus:outline-none focus:ring-2 focus:ring-purple-500/40 
-focus:border-purple-500 transition"
+focus:outline-none focus:ring-2 focus:ring-purple-500/40 focus:border-purple-500 transition"
                 ></textarea>
 
                 <button
                   type="submit"
-                  className="mt-6 py-4 rounded-xl font-medium text-white 
-  bg-linear-to-r from-purple-700 to-fuchsia-600
-  shadow-lg shadow-purple-500/30
-  hover:shadow-xl hover:scale-[1.02]
-  transition duration-300"
+                  className="py-3 rounded-xl font-medium text-white bg-linear-to-r from-purple-700 to-fuchsia-600 shadow-lg shadow-purple-500/30
+  hover:shadow-xl hover:scale-[1.02] transition duration-300 cursor-pointer"
                 >
                   Send Message →
                 </button>
               </form>
+              <div
+                className={`mt-4 p-4 rounded-lg text-white text-center 
+  transition-all duration-500 ${success ? "bg-green-500 scale-105" : "opacity-0 scale-95"}`}
+              >
+                Message Sent Successfully!
+              </div>
             </div>
           </div>
         </div>
